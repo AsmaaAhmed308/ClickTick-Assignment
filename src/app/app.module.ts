a08659a1d826ec69c3ab4c99f563672e4aa46544
ENG.ASMAA <asmaasarhan76@gmail.com>
2024-08-23 02:40:48 +0300

    login update work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,1 +1,21 @@
+import { NgModule } from '@angular/core';  
+import { BrowserModule } from '@angular/platform-browser';  
+import { provideHttpClient } from '@angular/common/http'; // Ensure this is included  
+import { AppComponent } from './app.component';  
+import { AppRoutingModule } from './app.routes';
+import { CommonModule } from '@angular/common';
 
+@NgModule({  
+  declarations: [  
+    AppComponent  
+  ],  
+  imports: [  
+    BrowserModule, 
+    AppRoutingModule 
+  ],  
+  providers: [  
+    provideHttpClient()  // Ensure this is set up correctly  
+  ],  
+  bootstrap: [AppComponent]  
+})  
+export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,30 +1,70 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs/internal/Subject';
-import { RestApiService } from '../rest-api-services/rest-api-serices.service';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule  
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, FormsModule, CommonModule], // Add HttpClientModule here  
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-
-
 export class LoginComponent {
-  //-------------------------------
   private ngUnsubscribe: Subject<void> = new Subject<void>();
-  email: string = '';  
-  password: string = '';  
-  errorMessage: string = '';  
-  //-------------------------------
-  constructor(  private router: Router,
-                private  _RestApiService  : RestApiService
-  ) {}
-  
-  signIn() {  
-     this._RestApiService.getLoginData()
-  }  
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+  loginUserData = "https://dummyjson.com/users";
+  responseObj = <any>{}
+  usersArr = []
+  loginUserAuthenticationData = <any>{}
+  showWarningMessage: boolean = false;
+  showDangerMessage: boolean = false;
+  constructor(private router: Router, private _HttpClient: HttpClient) { }
 
-} 
+  signIn() {
+
+    this.reset()
+    if (!this.email || !this.password) {
+      this.showWarningMessage = true;
+      return;
+    }
+    this._HttpClient.get(this.loginUserData).subscribe(response => {
+      if (response) {
+        this.responseObj = response
+        if (this.responseObj.users && this.responseObj.users.length > 0) {
+          this.usersArr = this.responseObj.users;
+          this.checkLoginUserAuthuntication()
+          console.log(response); // Handle the response  
+        }
+      }
+    }, error => {
+      console.error('Error fetching user data:', error);
+    });
+  }
+
+  reset(){
+    this.showWarningMessage  = false;
+    this.showDangerMessage  = false;
+    this.responseObj   = null
+    this.usersArr = []
+    this.loginUserAuthenticationData  = null
+  }
+
+  checkLoginUserAuthuntication() {
+    this.loginUserAuthenticationData = this.usersArr.find(user => user['email'] == this.email && user['password'] && this.password)
+    if (this.loginUserAuthenticationData) {
+      console.log('Succesful , Authentication is exisit')
+      this.showDangerMessage = false;
+    }
+
+    else {
+      console.log('warning , Authentication is not exisit')
+      this.showDangerMessage = true;
+    }
+  }
+}
