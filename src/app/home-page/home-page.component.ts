c067071af8f87d8b634c01f7dc8ae31f69b4e98a
AsmaaAhmed308 <asmaasarhan76@gmail.com>
2024-08-27 01:05:46 +0300

    commite -work

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -16,10 +16,12 @@ import { Product } from '../model/product-list.model'
 export class HomePageComponent {
   ProductDataUrl  = "https://dummyjson.com/products"
   ProductResponse  =<any>{}
+  AllProductsData: Product[] = [];  
   ProductList: Product[] = [];  
   categoryList = []
-  categories : string[] =[];  
+  categories : any[] =[];  
   selectedCategory: any
+  CartList  : Product[] = [];  
   constructor(private router: Router, private _HttpClient: HttpClient) { }
 
   ngOnInit() {  
@@ -47,20 +49,45 @@ export class HomePageComponent {
   }
 
  
-  getProductCategories() {  
-    this.ProductList.forEach(product => {  
-        const category = product['category'];  
-        // Check if the category already exists in the categories array  
-        if (!this.categories.includes(category)) {  
-            this.categories.push(category); // Push only if it doesn't exist  
-        }  
-    });  
-
-    console.log(' this.categories__' ,  this.categories)
-}
 
   handelProductList(){
-    this.ProductList   =  this.ProductResponse['products']
+    this.AllProductsData   =  this.ProductResponse['products']
+    this.ProductList  =  [...this.AllProductsData]
     this.getProductCategories()
   }
+
+  getProductCategories() {  
+    this.categories.push({ categoryName: 'All', categoryCount: this.AllProductsData.length })
+
+    this.AllProductsData.forEach(product => {  
+        const category = product['category'];   
+        
+        // Check if the categories array already contains an object with categoryName equal to the current category  
+        const categoryExists = this.categories.some(cat => cat.categoryName === category);  
+        
+        if (!categoryExists) {  
+            let categoryList = this.AllProductsData.filter(ele => ele.category === category);  
+            console.log(category, '----', categoryList);  
+            this.categories.push({ categoryName: category, categoryCount: categoryList.length }); // Push only if it doesn't exist   
+        }  
+    }); 
+    console.log(' this.categories_',  this.categories) 
+}
+
+
+filterProductsByCategory(category: any){
+    this.ProductList = []
+    this.ProductList  =  [...this.AllProductsData]
+    if(category.categoryName != 'All'){
+      this.ProductList =  this.ProductList.filter(ele => ele.category  ==  category.categoryName)  
+    }
+    console.log('category ___' ,   category)
+    console.log('AllProductsData ___' ,   this.AllProductsData)
+
+  }
+
+  addToCart(item: any) {  
+    this.CartList.push(item)
+    console.log('Item added to cart:', item);  
+}
 }
